2cb8a77f554aa9a1abe12964f439917637fe9778
Karen Kirr <kkirr77@yahoo.com>
2013-12-07 11:41:22 -0800

    updated gem

--- lib/kkfinal_gem.rb
+++ lib/kkfinal_gem.rb
@@ -1,15 +1,25 @@
-puts "I'm the final Gem!"
+
 
 class PhoneFormatter
 	def formatter
 		puts "phone formatter!"
 	end
 
-	def formatPhoneStrg (s = "string to format")
-		puts s
+	def formatPhoneStrg (num)
+	
+    digits = num.insert(-5,'-').insert(-9,'-')
+    if (digits.length==13 && digits[0] == '1')
+    	num.insert(-13, '-') end
+   return digits
 	end
 
 	def pass_args_back (hello)
-		hello
+		return hello
+
 	end
 end
+
+p = PhoneFormatter.new
+	p.formatter
+	puts p.formatPhoneStrg("17034515115")
+	puts p.pass_args_back ("hello")
--- spec/kkfinal_gem_spec.rb
+++ spec/kkfinal_gem_spec.rb
@@ -1,7 +1,12 @@
 require 'kkfinal_gem'
 
 describe PhoneFormatter do 
+	
+
 	it "should pass our arguments back" do
-		PhoneFormatter.new.pass_args_back("Hello").should eq "Hello"
+		PhoneFormatter.new.pass_args_back("Hello").should eq "Hello"	
+	end
+		it "should pass our arguments back" do
+			PhoneFormatter.new.formatPhoneStrg("17034515115").should eq "1-703-451-5115"
 	end
 end
